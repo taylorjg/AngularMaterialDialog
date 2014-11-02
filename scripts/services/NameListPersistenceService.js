899a6a1e64cd93ab93340325339e1858f90a522b
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-11-02 20:27:59 +0000

    Reformatted service code

--- scripts/services/NameListPersistenceService.js
+++ scripts/services/NameListPersistenceService.js
@@ -1,45 +1,47 @@
 /**
  * Created by jonathantaylor on 02/11/2014.
  */
-(function() {
+(function () {
 
     "use strict";
 
     angular.module("NameListApp")
-        .service("NameListPersistenceService", ["$timeout", "$q", "store", function($timeout, $q, store) {
+        .service(
+            "NameListPersistenceService",
+            ["$timeout", "$q", "store", function ($timeout, $q, store) {
 
-            var STORE_KEY_ITEMS = "items";
+                var STORE_KEY_ITEMS = "items";
 
-            var DEFAULT_ITEMS = [
-                {
-                    id: 1,
-                    firstName: "firstname1",
-                    lastName: "lastname1",
-                    email: "firstname1.lastname1@gmail.com"
-                },
-                {
-                    id: 2,
-                    firstName: "firstname2",
-                    lastName: "lastname2",
-                    email: "firstname2.lastname2@gmail.com"
-                }
-            ];
+                var DEFAULT_ITEMS = [
+                    {
+                        id: 1,
+                        firstName: "firstname1",
+                        lastName: "lastname1",
+                        email: "firstname1.lastname1@gmail.com"
+                    },
+                    {
+                        id: 2,
+                        firstName: "firstname2",
+                        lastName: "lastname2",
+                        email: "firstname2.lastname2@gmail.com"
+                    }
+                ];
 
-            this.getItems = function() {
-                var items = store.get(STORE_KEY_ITEMS) || DEFAULT_ITEMS;
-                return $q.when(items);
-            };
+                this.getItems = function () {
+                    var items = store.get(STORE_KEY_ITEMS) || DEFAULT_ITEMS;
+                    return $q.when(items);
+                };
 
-            this.saveItems = function (items) {
-                var deferred = $q.defer();
-                $timeout(function() {
-                    // We do this in a timeout function to allow the digest loop to run
-                    // and update the $$hashKey properties of any recently added items.
-                    store.set(STORE_KEY_ITEMS, items);
-                    deferred.resolve(items);
-                });
-                return deferred.promise;
-            };
+                this.saveItems = function (items) {
+                    var deferred = $q.defer();
+                    $timeout(function () {
+                        // We do this in a timeout function to allow the digest loop to run
+                        // and update the $$hashKey properties of any recently added items.
+                        store.set(STORE_KEY_ITEMS, items);
+                        deferred.resolve(items);
+                    });
+                    return deferred.promise;
+                };
 
-        }]);
+            }]);
 }());
