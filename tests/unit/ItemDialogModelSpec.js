6e5de8426236c2b64e5e11e236c4539bd8d4fee0
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-11-05 14:27:42 +0000

    Added unit tests for ItemDialogModel

---
+++ tests/unit/ItemDialogModelSpec.js
@@ -0,0 +1,23 @@
+/**
+ * Created by taylojo on 05/11/14.
+ */
+(function() {
+
+    "use strict";
+
+    describe("ItemDialogModel unit tests", function() {
+
+        it("is initialised correctly when item is null", function() {
+            var model = window.nameListApp.models.itemDialogModel(null);
+            expect(model.item).toEqual({});
+            expect(model.title).toBe("Add Item");
+        });
+
+        it("is initialised correctly when item is not null", function() {
+            var item = {id: 3, firstName: "f3", lastName: "l3", email: "e3"};
+            var model = window.nameListApp.models.itemDialogModel(item);
+            expect(model.item).toBe(item);
+            expect(model.title).toBe("Edit Item 3");
+        });
+    });
+}());
